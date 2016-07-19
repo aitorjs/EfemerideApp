8d651424c1ba7d330b68b80cd376aca232941d98
Aitor Ibañez <freelance@aitoribanez.com>
2016-07-19 20:38:42 +0200

    json wikipedia response modified

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 let utils = require('./utils')
 let fetchData = require('./fetchData')
-// let database = require('./database')
+let database = require('./database')
 
 // let year = 2016
 let month = 7 // TODO: meterlo en for para que funcione por año ???
@@ -11,19 +11,7 @@ let lang = 'es'
 let events = {}
 // let lastDays = utils.getLastDayOfMonth(year)
 
-/* database.createTable().then(function (db) {
-  database.insert({
-    day: 2,
-    data: [{
-      date: 28,
-      data: 'Lorem'
-    },
-    {
-      date: 29,
-      data: 'Ipsum ipsum'
-    }]
-  })
-})
+database.createTable().then(function (db) {})
 
 setTimeout(function () {
   database.getsByDay(2, function (err, res) {
@@ -31,7 +19,7 @@ setTimeout(function () {
     let data = JSON.parse(res[0]['data'])
     console.log(data[0]['date'])
   })
-}, 3000) */
+}, 7000)
 
 start()
 
@@ -42,9 +30,8 @@ function start () {
   for (let day = 1; day <= 1; day++) {
     fetchData.fetch(day, monthT, lang, events).then(function (res) {
       console.log(res)
-      events[res.day] = res
+      database.insert(res)
     })
-  }
 
   // setTimeout(function () { console.log(events) }, 3000)
 }
