64fd72215cfbcbf2b40e57cb947f3404226c5d4c
Aitor Ibañez <freelance@aitoribanez.com>
2016-07-22 20:30:57 +0200

    EfemerideList, EfemerideListHeader and EfemerideLstFooter added with their own styles

--- components/EfemerideList.js
+++ components/EfemerideList.js
@@ -2,12 +2,17 @@ import React from 'react'
 
 import EfemerideListHeader from './EfemerideListHeader'
 import EfemerideListFooter from './EfemerideListFooter'
+import EfemerideListItems from './EfemerideListItems'
+import EfemerideListItem from './EfemerideListItem'
 import './efemerideList.scss'
 
 export default class EfemerideList extends React.Component {
   render () {
     return <div className='container' id='list'>
       <EfemerideListHeader />
+      <EfemerideListItems>
+        <EfemerideListItem />
+      </EfemerideListItems>
       <EfemerideListFooter />
     </div>
   }
